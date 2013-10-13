758e47b907f852c91f24a75cc671f18314c90f26
mdietz94 <mdietz@wesleyan.edu>
2013-10-13 18:16:55 -0400

    added drawing code

--- src/Player.hs
+++ src/Player.hs
@@ -3,48 +3,58 @@ module Player where
 import Base.GraphicsManager
 import Base.InputHandler
 import Base.Geometry
-import Graphics.UI.SDL (SDLKey(..))
+import Graphics.UI.SDL (SDLKey(..), Surface)
 
-data Player = Player { bounding :: Shape, velocity :: (Int,Int), alive :: Bool } deriving Show
+data Player = Player { bounding :: Shape, velocity :: (Int,Int), alive :: Bool, image :: IO Surface }
 playerHeight = 10
 playerWidth = 5
 
 initialize :: (Int,Int) -> Player
-initialize (x,y) = Player (Rectangle x y playerWidth playerHeight) (0,0) True
+initialize (x,y) = Player (Rectangle x y playerWidth playerHeight) (0,0) True (loadImage "dot.bmp" Nothing)
 
 update :: KeyboardState -> [Shape] -> Player -> Player
-update kS rects player@(Player _ (dx,dy) _ ) = if isOnGround && (isPressed kS SDLK_UP) then player5 { velocity=(dx',-10) } else player5
+update kS rects player@(Player _ (dx,dy) _  _) = if isOnGround && (isPressed kS SDLK_UP) then player5 { velocity=(dx',-10) } else player5
     where
-        (player5@(Player _ (dx',dy') _),isOnGround) = checkCollisionV rects player4
+        (player5@(Player _ (dx',dy') _ _),isOnGround) = checkCollisionV rects player4
         player4 = moveV player3
         player3 = checkCollisionH rects player2
         player2 = moveH player1
         player1 = player { velocity=(dx+strafe,dy+2) }
         strafe = (if isDown kS SDLK_LEFT then -5 else 0) + (if isDown kS SDLK_RIGHT then 5 else 0)
 
+draw :: Surface -> Player -> IO ()
+draw screen (Player (Rectangle x y _ _)  _ _ image) = do
+    img <- image
+    drawImage screen img (x,y)
+    return ()
+
 moveH :: Player -> Player
-moveH (Player (Rectangle x y w h) (dx,dy) a) = (Player (Rectangle (x+dx) (y) w h) (dx,dy) a)
+moveH p@(Player (Rectangle x _ _ _) (dx,_) _ _) = p { bounding=newRect }
+    where
+        newRect = (bounding p) { rectX=x+dx }
 
 moveV :: Player -> Player
-moveV (Player (Rectangle x y w h) (dx,dy) a) = (Player (Rectangle (x) (y+dy) w h) (dx,dy) a)
+moveV p@(Player (Rectangle _ y _ _) (_,dy) _ _) = p { bounding=newRect } 
+    where
+        newRect = (bounding p) { rectY=y+dy }
 
 checkCollisionH :: [Shape] -> Player -> Player
 checkCollisionH [] p = p
-checkCollisionH (r:rs) p@(Player pr@(Rectangle x y w h) (dx,dy) a)
+checkCollisionH (r:rs) p@(Player pr@(Rectangle x y w h) (dx,dy) a i)
     | dx == 0 = p
     | dx > 0 = if r `isRight` pr
-        then Player (pr { rectX=((rectX r)-1) }) (0,dy) a
+        then Player (pr { rectX=((rectX r)-1) }) (0,dy) a i
         else checkCollisionH rs p
     | otherwise = if r `isLeft` pr
-        then Player (pr { rectX=((rectX r) + (rectW r) + 1)}) (0,dy) a
+        then Player (pr { rectX=((rectX r) + (rectW r) + 1)}) (0,dy) a i
         else checkCollisionH rs p
 checkCollisionV :: [Shape] -> Player -> (Player,Bool)
 checkCollisionV [] p = (p,False)
-checkCollisionV (r:rs) p@(Player pr@(Rectangle x y w h) (dx,dy) a)
+checkCollisionV (r:rs) p@(Player pr@(Rectangle x y w h) (dx,dy) a i)
     | dy == 0 = (p, False)
     | dy > 0 = if r `isBelow` pr
-        then (Player (pr { rectY=((rectY r)-1) }) (dx,2) a, True)
+        then (Player (pr { rectY=((rectY r)-1) }) (dx,2) a i, True)
         else checkCollisionV rs p
     | otherwise = if r `isOnTop` pr
-        then (Player (pr { rectY=((rectY r) + (rectH r) + 1)}) (dx,2) a, False)
+        then (Player (pr { rectY=((rectY r) + (rectH r) + 1)}) (dx,2) a i, False)
         else checkCollisionV rs p
