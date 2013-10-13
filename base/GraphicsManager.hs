83679e7e0726db786ba71f0402bbe1a9001f2347
mdietz94 <mdietz@wesleyan.edu>
2013-10-13 15:19:12 -0400

    Added GraphicsManager

---
+++ base/GraphicsManager.hs
@@ -0,0 +1,53 @@
+module HaskellGame.GraphicsManager where
+
+import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.Image as SDL
+import qualified Graphics.UI.SDL.TTF as SDL
+import Data.Word(Word8)
+import Control.Monad(liftM)
+
+type Width = Int
+type Height = Int
+type Title = String
+type Pos = (Int,Int)
+
+screenWidth  = 640
+screenHeight = 480
+screenBpp    = 32
+
+loadImage :: String -> Maybe (Word8, Word8, Word8) -> IO SDL.Surface
+loadImage filename colorKey = SDL.load filename >>= SDL.displayFormat >>= setColorKey' colorKey
+
+setColorKey' Nothing s = return s
+setColorKey' (Just (r, g, b)) surface = (SDL.mapRGB . SDL.surfaceGetPixelFormat) surface r g b >>= SDL.setColorKey surface [SDL.SrcColorKey] >> return surface
+
+drawImage :: SDL.Surface -> SDL.Surface -> Pos -> IO Bool
+drawImage screen img (x,y) = SDL.blitSurface img Nothing screen offset
+    where
+        offset = Just SDL.Rect { SDL.rectX = x, SDL.rectY = y, SDL.rectW = 0, SDL.rectH = 0 }
+
+loadFont :: String -> Int -> IO SDL.Font
+loadFont = SDL.openFont
+
+drawText :: SDL.Surface -> SDL.Font -> String -> Pos -> (Int,Int,Int) -> IO Bool
+drawText screen font text pos (r,g,b) = do
+    surface <- SDL.renderTextSolid font text (SDL.Color (fromIntegral r) (fromIntegral g) (fromIntegral b))
+    drawImage screen surface pos
+
+initialize :: Width -> Height -> Title -> IO SDL.Surface
+initialize w h title = do
+    screen <- SDL.setVideoMode w h screenBpp [SDL.SWSurface]
+    SDL.setCaption title []
+    return screen
+    
+
+begin :: SDL.Surface -> IO ()
+begin screen = do
+    jrect <- Just `liftM` SDL.getClipRect screen
+    color <- (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0xff 0xff 0xff
+    SDL.fillRect screen jrect color
+    return ()
+
+end :: SDL.Surface -> IO ()
+end screen = do
+    SDL.flip screen
