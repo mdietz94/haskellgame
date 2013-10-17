bdef978eebf0929391b5046f8b657bc4199dd181
mdietz94 <mdietz@wesleyan.edu>
2013-10-16 21:27:23 -0400

    now all players have to reach the end.

--- src/Level.hs
+++ src/Level.hs
@@ -49,20 +49,11 @@ data Level = Level {
 }
 
 initialize :: Int -> Level
-initialize 0 = Level 0 sPos (Geo.Point 300 220) platforms mPlatforms (C.FixedCamera (0,0) height width) (map P.initialize sPos) 0 []
+initialize 0 = Level 0 sPos (Geo.Rectangle 350 240 20 20) platforms mPlatforms (C.FixedCamera (0,0) height width) (map P.initialize sPos) 0 []
     where
         sPos = [(0,200), (100,200)]
-        mPlatforms = [(Platform.moveableInitialize (0,100) (100,100) 30 10 5 (0,0,0) )]
+        mPlatforms = [(Platform.moveableInitialize (0,200) (100,200) 30 10 5 (0,0,0) )]
         platforms = [(Platform.Platform (Geo.Rectangle 0 300 400 30) (0,0,0)),(Platform.Platform (Geo.Rectangle 450 290 300 30) (0,0,0)), (Platform.Platform (Geo.Rectangle 300 260 95 10) (0,0,0))]
-
-{-
-
-We need to differentiate between objects, since right now objects of the same
-type cannot collide because otherwise they will always detect collisions against
-themselves.  We could just check to see if the objects are equivalent, though
-working around the indices is probably a better idea.
-
--}
 
 update :: IH.KeyboardState -> Level -> (Bool,Level)
 update kS lev = if IH.isDown kS SDL.SDLK_LSHIFT then (False,head (levelHistory lev)) else (not . (elem False) . (map (Geo.collides (goal nL))) $ pObs, nL)
@@ -71,13 +62,10 @@ update kS lev = if IH.isDown kS SDL.SDLK_LSHIFT then (False,head (levelHistory l
         playInd = max 0 (min pI ((length (players lev)) - 1))
         pI = (curPlayer lev) + (if IH.isDown kS SDL.SDLK_q then (-1) else 0) + (if IH.isDown kS SDL.SDLK_e then 1 else 0) 
         sObs = (map Platform.bounding $ staticObstacles lev)
-        mObs = (map Platform.mBounding $ movingObstacles lev)
         pObs = (map P.bounding $ players lev)
-        skip :: Int -> [a] -> [a]
-        skip _ [] = []
-        skip 0 (x:xs) = xs
-        skip n (x:xs) = x : (skip (n-1) xs)
+        -- lengthy way to exclude players from hitting themselves
         newPlayers = [ P.update (if n == playInd then kS else []) (sObs ++ mObs ++ (skip n pObs)) ((players lev)!!n) | n <- [0..((length . players $ lev) - 1)] ]
+        mObs = (map Platform.mBounding $ newObstacles)
         newObstacles = map Platform.update (movingObstacles lev)
 
 draw :: SDL.Surface -> Level -> IO ()
@@ -86,5 +74,12 @@ draw screen lev = do
     sequence $ map (Platform.draw screen (camera lev)) (staticObstacles lev)
     sequence $ map (P.draw screen (camera lev)) (players lev)
     sequence $ map (Platform.draw screen (camera lev)) (movingObstacles lev)
-    G.drawRect screen ((\p -> (Geo.ptX p) - 5) . goal $ lev, (\p -> (Geo.ptY p) - 5) . goal $ lev) 10 10 (50,100,50)
+    G.drawRect screen (Geo.rectX . goal $ lev, Geo.rectY . goal $ lev) (Geo.rectW . goal $ lev) (Geo.rectH . goal $ lev) (50,100,50)
     return ()
+
+-- Helper function
+
+skip :: Int -> [a] -> [a]
+skip _ [] = []
+skip 0 (x:xs) = xs
+skip n (x:xs) = x : (skip (n-1) xs)
