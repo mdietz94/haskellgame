4bbf51717ddcd7ba40934e0c4534a8dd4fe258d1
mdietz94 <mdietz@wesleyan.edu>
2013-10-23 19:56:40 -0400

    Added basic switch.  just moves the camera, which doesnt work as expected because Justin hasn\'t fixed it yet.  Also the update only works for hold switches now

--- src/Level.hs
+++ src/Level.hs
@@ -5,6 +5,7 @@ import Control.Monad
 import Control.Monad.State
 import qualified Player as P
 import qualified Platform as Platform
+import qualified Switch as Switch
 import qualified Base.InputHandler as IH
 import qualified Base.AudioManager as AM
 import qualified Base.GraphicsManager as G
@@ -21,6 +22,7 @@ data LevelConfig = LevelConfig {
     startPos :: [(Int,Int)],
     goal :: Geo.Shape,
     staticObstacles :: [Platform.Platform],
+    holdSwitches :: [Switch.Switch LevelData],
     currMusic :: SDL.Music
 }
 
@@ -40,7 +42,7 @@ data Level = Level {
 initialize :: Int -> IO Level
 initialize 0 = do
     m <- SDL.loadMUS "../assets/music/Aalborn_Pulse.mp3"
-    let l = Level (LevelConfig 0 sPos (Geo.Rectangle 350 240 20 20) platforms m) (LevelData mPlatforms (C.FixedCamera (0,0) height width) (map P.initialize sPos) 0 [])
+    let l = Level (LevelConfig 0 sPos (Geo.Rectangle 350 240 20 20) platforms [(Switch.Hold (Geo.Rectangle 250 290 30 10) id (\l@(LevelData _ fc@(C.FixedCamera (x,y) _ _) _ _ _) -> l { camera=(fc { C.corner=(x+1,y) } ) }))] m) (LevelData mPlatforms (C.FixedCamera (0,0) height width) (map P.initialize sPos) 0 [])
     AM.playMusic m
     return l
     where
@@ -48,10 +50,11 @@ initialize 0 = do
         mPlatforms = [(Platform.moveableInitialize (10,200) (100,250) 30 10 5 (0,0,0) )]
         platforms = [(Platform.Platform (Geo.Rectangle 0 300 400 30) (0,0,0)),(Platform.Platform (Geo.Rectangle 450 290 300 30) (0,0,0)), (Platform.Platform (Geo.Rectangle 300 260 95 10) (0,0,0))]
 
-update :: IH.KeyboardState -> Level -> (Bool,Level)
+{-update :: IH.KeyboardState -> Level -> (Bool,Level)
 update kS l@(Level lC lev) = if IH.isDown kS SDL.SDLK_LSHIFT then (False,l { lD=(head (levelHistory lev)) }) else (not . (elem False) . (map (Geo.collides (goal lC))) $ pObs, l { lD=nL })
     where
-        nL = lev { players=newPlayers, levelHistory=(lev:(levelHistory lev)), curPlayer=playInd, movingObstacles=newObstacles }
+        nL = map (Switch.update nL' newPlayers) (holdSwitches lC)
+        nL' = lev { players=newPlayers, levelHistory=(lev:(levelHistory lev)), curPlayer=playInd, movingObstacles=newObstacles }
         playInd = max 0 (min pI ((length (players lev)) - 1))
         pI = (curPlayer lev) + (if IH.isDown kS SDL.SDLK_q then (-1) else 0) + (if IH.isDown kS SDL.SDLK_e then 1 else 0) 
         sObs = (map Platform.bounding $ staticObstacles lC)
@@ -60,6 +63,39 @@ update kS l@(Level lC lev) = if IH.isDown kS SDL.SDLK_LSHIFT then (False,l { lD=
         newPlayers = [ P.update (if n == playInd then kS else []) (sObs ++ mObs ++ (skip n pObs)) newObstacles ((players lev)!!n) | n <- [0..((length . players $ lev) - 1)] ]
         mObs = (map Platform.mBounding $ newObstacles)
         newObstacles = map Platform.update (movingObstacles lev)
+-}
+
+update :: IH.KeyboardState -> Level -> (Bool,Level)
+update kS l@(Level lC lev) = (won,l { lD=nLD })
+    where
+        (won,nLD) = runState (updateS kS lC) lev
+
+updateS :: IH.KeyboardState -> LevelConfig -> State LevelData Bool
+updateS kS lC = do
+    prev <- gets levelHistory
+    if ((IH.isDown kS SDL.SDLK_LSHIFT) && (not . null $ prev))
+        then do
+            put . head $ prev
+            return False
+        else do
+            obs <- gets $ (map Platform.update) . movingObstacles
+            modify $ \s -> s { movingObstacles=obs }
+            let mObs = map Platform.mBounding obs
+            let sObs = (map Platform.bounding) . staticObstacles $ lC
+            pObs <- gets $ (map P.bounding) . players
+            pls <- gets players
+            currP <- gets curPlayer
+            let playInd = max 0 (min (currP + (if IH.isDown kS SDL.SDLK_q then (-1) else 0) + (if IH.isDown kS SDL.SDLK_e then 1 else 0)) ((length pls) - 1))
+            lev <- get
+            let newPlayers = [ P.update (if n == playInd then kS else []) (sObs ++ mObs ++ (skip n pObs)) obs ((players lev)!!n) | n <- [0..((length . players $ lev) - 1)] ]
+            modify $ \t -> t { players=newPlayers, levelHistory=(t:(levelHistory t)), curPlayer=playInd, movingObstacles=obs }
+            modify $ \t -> swUpdate t (holdSwitches lC)
+            return $ not . (elem False) . (map (Geo.collides (goal lC))) $ pObs
+    
+
+swUpdate :: LevelData -> [Switch.Switch LevelData] -> LevelData
+swUpdate x [] = x
+swUpdate lev (x:xs) = swUpdate (snd $ Switch.update lev (players lev) x) xs
 
 draw :: SDL.Surface -> Level -> IO ()
 draw screen (Level lC lev) = do
@@ -67,6 +103,7 @@ draw screen (Level lC lev) = do
     sequence $ map (Platform.draw screen (camera lev)) (staticObstacles lC)
     sequence $ map (P.draw screen (camera lev)) (players lev)
     sequence $ map (Platform.draw screen (camera lev)) (movingObstacles lev)
+    sequence $ map (Switch.draw screen (camera lev)) (holdSwitches lC)
     G.drawRect screen (Geo.rectX . goal $ lC, Geo.rectY . goal $ lC) (Geo.rectW . goal $ lC) (Geo.rectH . goal $ lC) (50,100,50)
     -- Need to tell the GC to not free the music (SDL should really do this)
     touchForeignPtr . currMusic $ lC
--- src/Switch.hs
+++ src/Switch.hs
@@ -4,34 +4,33 @@ import Graphics.UI.SDL(Surface)
 import Base.GraphicsManager(drawRect)
 import Base.Geometry(Shape(Rectangle),collides)
 import Base.Camera(FixedCamera,gameToScreen)
-import Level(LevelData(..))
 import Player(Player,bounding)
 
 
-data Switch = Tap { tapBounding :: Shape, tapOn :: (LevelData -> LevelData), tapOff :: (LevelData -> LevelData), tapHeld :: Bool }
-	| Hold { holdBounding :: Shape, holdOn :: (LevelData -> LevelData), holdOff :: (LevelData -> LevelData) }
+data Switch a = Tap { tapBounding :: Shape, tapOn :: (a -> a), tapOff :: (a -> a), tapHeld :: Bool }
+	| Hold { holdBounding :: Shape, holdOn :: (a -> a), holdOff :: (a -> a) }
 
-update :: LevelData -> Switch -> (Switch,LevelData)
-update lD@(LevelData _ _ players _ _) t@(Tap r on off held) = if held == (not . null . filter (==True) . map ((collides r) . bounding) $ players)
-	then (t,lD)
+update :: a -> [Player] -> Switch a -> (Switch a,a)
+update lD players t@(Tap r on off held) = if held == (not . null . filter (==True) . map ((collides r) . bounding) $ players)
+	then (t, (if held then on else off) $ lD)
 	else (t { tapHeld=(not held) }, (if held then off else on) $ lD)
-update lD@(LevelData _ _ players _ _) t@(Hold r on off) = if (null . filter (==True) . map ((collides r) . bounding) $ players)
+update lD players t@(Hold r on off) = if (null . filter (==True) . map ((collides r) . bounding) $ players)
 	then (t,on lD)
 	else (t,off lD)
 
 
-draw :: Surface -> FixedCamera -> Switch -> IO ()
+draw :: Surface -> FixedCamera -> Switch a -> IO ()
 draw screen cam (Tap (Rectangle x y w h) _ _ _) = do
     let pt = gameToScreen cam 640 480 (x,y)
     let wh = gameToScreen cam 640 480 (w,h)
     case (pt,wh) of
-        (Just x', Just (w',h')) -> drawRect screen x' w' h' (10,10,10)
+        (Just x', Just (w',h')) -> drawRect screen x' w' h' (100,10,10)
         _ -> return ()
     return ()
 draw screen cam (Hold (Rectangle x y w h) _ _) = do
     let pt = gameToScreen cam 640 480 (x,y)
     let wh = gameToScreen cam 640 480 (w,h)
     case (pt,wh) of
-        (Just x', Just (w',h')) -> drawRect screen x' w' h' (10,10,10)
+        (Just x', Just (w',h')) -> drawRect screen x' w' h' (100,10,10)
         _ -> return ()
     return ()
